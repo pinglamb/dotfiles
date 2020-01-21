a823e8b1514689803372f191f40559adf8f91575
pinglamb <pinglamb@pinglambs-MacBook-Pro.local>
2020-01-21 16:17:08 +0800

    🔧 Added some dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+fundle plugin 'edc/bass'
+fundle plugin 'brgmnn/fish-docker-compose'
+fundle plugin 'oh-my-fish/plugin-git-flow'
+fundle plugin 'jhillyerd/plugin-git'
+fundle plugin 'rbenv/fish-rbenv'
+
+fundle plugin 'fisherman/getopts'
+fundle plugin 'fisherman/z'
+
+fundle plugin 'fisherman/git_util'
+# fundle plugin 'fisherman/last_job_id'
+# fundle plugin 'fisherman/humanize_duration'
+fundle plugin 'oh-my-fish/theme-default'
+
+fundle init
+
+set fish_pager_color_progress cyan
+
+set -x EDITOR vim
+
+# The next line updates PATH for the Google Cloud SDK.
+# if [ -f '/Users/pinglamb/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/pinglamb/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/pinglamb/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+
+# set -gx PATH /Users/pinglamb/go/bin $PATH
+# set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
+ 
+# set -x GOPATH /Users/pinglamb/go
+# set -x GOBIN /Users/pinglamb/go/bin
---
+++ MAC.md
@@ -0,0 +1,8 @@
+```
+brew install git
+brew install postgresql
+brew install rbenv
+brew cask install homebrew/cask-fonts/font-hack
+brew install node
+brew install yarn
+```
